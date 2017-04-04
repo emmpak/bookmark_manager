a258c64b9e0226c5a04c5243247393c96ada859c
Vivien Tang <vivientang@Viviens-MacBook-Pro.local>
2017-04-04 16:53:16 +0100

    add Tag model

---
+++ spec/features/add_tags_spec.rb
@@ -0,0 +1,14 @@
+feature 'add tags' do
+  scenario 'to links' do
+    # Tag.create(name: 'search', link_id: 'Makers Academy')
+    visit('/links')
+    click_button 'new'
+    fill_in 'Title', with: 'Google'
+    fill_in 'Link', with: 'www.google.com'
+    fill_in 'Tag', with: 'search'
+    click_button 'submit'
+    within 'ul#links' do
+      expect(page).to have_content('Google www.google.com search')
+    end
+  end
+end
