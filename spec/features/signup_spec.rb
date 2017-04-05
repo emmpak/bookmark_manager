179c93a5057bd1295a9c22f2fc7da5f6239091a9
Edward Ward <edwardwardward@gmail.com>
2017-04-05 16:52:34 +0100

    Add password confirmation

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -3,9 +3,19 @@ feature 'User Signs up' do
     visit '/users/new'
     fill_in 'email', with: "bob@gmail.com"
     fill_in 'password', with: '1234'
+    fill_in 'password_confirmation', with: '1234'
     click_button 'Submit'
     expect(current_path).to eq ('/links')
     expect(page).to have_content('Welcome, bob@gmail.com')
     expect(User.count).to eq 1
+  end
+
+  scenario 'signup with wrong password' do
+    visit '/users/new'
+    fill_in 'email', with: "bob@gmail.com"
+    fill_in 'password', with: '1234'
+    fill_in 'password_confirmation', with: '12345'
+    click_button 'Submit'
+    expect(User.count).to eq 0
   end
 end
